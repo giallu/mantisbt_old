0d3aced726390d24362c235680bb29336896c506
Gianluca Sforna <giallu@gmail.com>
2009-01-23 00:37:24 +0100

    Adapt inclusion code to new layout

--- public/api/soap/mantisconnect.php
+++ public/api/soap/mantisconnect.php
@@ -6,13 +6,9 @@
 # change the license of future releases.
 # See docs/ folder for more details
 
-$t_current_dir = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
-$t_nusoap_dir  = $t_current_dir . 'nusoap';
-
+require_once( '../../setenv.php' );
 # includes nusoap classes
-chdir( $t_nusoap_dir );
-require_once( 'nusoap.php' );
-chdir( $t_current_dir );
+require_once( 'nusoap/nusoap.php' );
 
 # create server
 $l_oServer = new soap_server();
