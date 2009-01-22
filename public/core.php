6911a54275ba8924fcb08aa8d95b1741cf081940
Gianluca Sforna <giallu@gmail.com>
2009-01-22 01:22:52 +0100

    clean up include_path

--- public/core.php
+++ public/core.php
@@ -38,9 +38,9 @@
 
 	define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application/'));
 	set_include_path(
-		APPLICATION_PATH . '/core' 
-		. PATH_SEPARATOR . APPLICATION_PATH . '/../library'
-		. PATH_SEPARATOR . get_include_path()
+		APPLICATION_PATH . '/core' . PATH_SEPARATOR .
+		APPLICATION_PATH . '/../library' . PATH_SEPARATOR .
+		get_include_path()
 	);
 
 	
